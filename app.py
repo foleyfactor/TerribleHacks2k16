e84e66aa20a9b046c2a35b8af16eac54d6298576
Ethan Guo <ethguo@gmail.com>
2016-11-26 13:26:20 -0500

    Added twitch interface, hopefully

--- app.py
+++ app.py
@@ -1,19 +1,44 @@
+import env
+
 from flask import Flask
 from flask import redirect
 from flask import render_template
 from flask import request
 
-import os
 import requests
 
+from twitch import Twitch
+
+trueStrings = [">", "y", "Y"]
+falseStrings = ["<", "n", "N"]
 
 app = Flask(__name__)
 
-# ENV_VAR = os.environ["ENV_VAR"]
+t = Twitch()
+t.twitch_connect(env.TWITCH_USERNAME, env.TWITCH_KEY)
 
 @app.route("/")
 def getIndex():
 	return render_template("index.html")
+
+@app.route("/twitch")
+def getTwitch():
+	messages = t.twitch_recieve_messages();
+
+	trues, falses = 0, 0
+
+	for message in messages:
+		text = message["message"]
+		print(message["username"] + ":", text)
+		if text in trueStrings:
+			trues += 1
+		elif text in falseStrings:
+			falses += 1
+
+	print("trues:", trues, "\tfalses:", falses)
+
+	return "true" if trues > falses else "false"
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
